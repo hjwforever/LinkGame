b9312bd5ab1665c74e12f26ee21ff2ef716d6d7c
MakerHu <1003964217@qq.com>
2020-05-21 15:05:59 +0800

    胡江浩修改main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "mahjong.h"
 
 #include <QApplication>
+#include<iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+    cout<<"»Æ¿¡ö©Ì«Ç¿ÁË£¡"<<endl;
     QApplication a(argc, argv);
     Mahjong w;
     w.show();
