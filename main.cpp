7f2e29ec5a79e2e5d86adf43f744ee092a0a987e
MakerHu <1003964217@qq.com>
2020-05-21 15:49:25 +0800

    胡江浩修改main_3

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout<<"hjwnb!!!!"<<endl;
+    cout<<"hjwnb!!!!hhh"<<endl;
     QApplication a(argc, argv);
     Mahjong w;
     w.show();
